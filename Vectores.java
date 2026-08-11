31225d2cba3e40f94b44426ec4d88df6fdd0e1ae
bl23z <juniorjulian2030@gmail.com>
2026-08-11 18:31:02 -0500

    vectores

---
+++ Vectores.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.vectores;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author USER
+ */
+public class Vectores {
+
+    public static void main(String[] args) {
+       Scanner teclado=new Scanner(System.in);
+       int x[]={1,3,10,11};
+       
+       System.out.println("posicion 0 contiene"+ x[0]);
+       System.out.println("posicion 1 contiene"+ x[1]);
+       System.out.println("posicion 2 contiene"+ x[2]);
+       System.out.println("posicion 3contiene"+ x[3]);
+    }
+}
